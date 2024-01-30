1d11cd0a72a27de6903509631c903f8b32b6e5d4
mobin nakhjavani <mobin.ravoon@gmail.com>
2024-01-30 16:15:37 +0330

    create project

---
+++ src/function_header.c
@@ -0,0 +1,15 @@
+#include "header.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#ifndef __HEADER__
+#define __HEADER__
+void print(){
+    printf("Hello World!\n");
+}
+#endif
